7e7b902c83c151b29e988eda823e5eca4c66dbbd
Juneict <junemoenyinyiict@gmail.com>
2025-01-16 21:55:35 +0700

    add: connect db change

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 const connect = async () => {
-    return await mongoose.connect(process.env.MONGO_URI,{
-    })
+    const url = process.env.MONGO_URI;
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
 }
 
 module.exports = connect;
